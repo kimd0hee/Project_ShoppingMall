ca06b7f3dd8422322e9029d085fbe10a4f64d87c
woori <woori@DESKTOP-3T7OJU9>
2023-04-11 11:49:01 +0900

    update

--- shopping/src/main/java/com/admin/Controller/AdminController.java
+++ shopping/src/main/java/com/admin/Controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.admin.controller;
+package com.admin.Controller;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
--- shopping/src/main/java/com/board/dao/BoardDAO.java
+++ shopping/src/main/java/com/board/dao/BoardDAO.java
@@ -7,20 +7,16 @@ import com.board.dto.BoardVO;
 public interface BoardDAO {
 	
 	public void insertBoard(BoardVO vo) throws Exception;
-	
-<<<<<<< HEAD
-	public BoardVO viewBoard(int cs_id) throws Exception;
-=======
+
+
 	public BoardVO viewBoard(int board_id) throws Exception;
->>>>>>> branch 'K.D.H' of https://github.com/kimd0hee/Project_ShoppingMall.git
+
 	
 	public void updateBoard(BoardVO vo) throws Exception;
 	
-<<<<<<< HEAD
-	public void deleteBoard(int cs_id) throws Exception;
-=======
+
 	public void deleteBoard(int board_id) throws Exception;
->>>>>>> branch 'K.D.H' of https://github.com/kimd0hee/Project_ShoppingMall.git
+
 	
 	public List<BoardVO> boardList() throws Exception;
 }
